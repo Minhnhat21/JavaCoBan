2def4ce978d91c2023fd1db7a35a56c50ce68a44
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2021-09-04 19:34:24 +0700

    update file contact

---
+++ src/javaapplication/Student.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+import java.util.Scanner;
+/**
+ *
+ * @author nguye
+ */
+public class Student {
+    Scanner sc = new Scanner(System.in);
+    int acount = sc.nextInt();
+}
