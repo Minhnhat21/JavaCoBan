e71357453209c13dc425289c346d9f8c0420a9ac
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2021-09-16 10:00:40 +0700

    new commit file

---
+++ src/Baitap/Bai05.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Baitap;
+
+/**
+ *
+ * @author nguye
+ */
+import java.util.Scanner;
+public class Bai05 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập hệ số a: ");
+        int a = sc.nextInt();
+        System.out.print("Nhập hệ số b: ");
+        int b = sc.nextInt();
+        // gọi x là nghiệm cảu phương trình
+        
+        double x = (double) -b/a;
+        
+        System.out.println("Nghiệm của phương trình bậc nhất 1 ẩn là: " + x);
+    }
+ 
+}
---
+++ src/Baitap/Bai06.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Baitap;
+
+/**
+ *
+ * @author nguye
+ */
+import java.util.Scanner;
+public class Bai06 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập hệ số a: ");
+        int a = sc.nextInt();
+        System.out.print("Nhập hệ số b: ");
+        int b = sc.nextInt();
+        System.out.print("Nhập hệ số c: ");
+        int c = sc.nextInt();
+        int delta = b*b - 4*a*c;
+        if (delta > 0) {
+            // gọi x1, x2 là nghiệm của phương trình
+            double x1;
+            double x2;
+            System.out.println("Phương trình có 2 nghiệm phân biệt");
+            x1 = (double) (-b + Math.sqrt(delta)) / (2.0*a); 
+            x2 = (double) (b + Math.sqrt(delta)) / (2.0*a);
+            System.out.println("x1 = " + x1 +"\n" + "x2 = " + x2);
+        } else if (delta == 0) {
+            System.out.println("Phương trình có nghiệm duy nhất");
+            double x = - b / (2*a);
+            System.out.println("x = " + x);
+        } else {
+            System.out.println("Phương trình vô nghiệm");
+        }
+                
+    }
+}
