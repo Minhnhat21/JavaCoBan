fe2f6c3401601484a2474d6153071fc05b50823c
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2021-09-16 10:16:38 +0700

    update file

--- src/Baitap/Bai05.java
+++ src/Baitap/Bai05.java
@@ -18,10 +18,15 @@ public class Bai05 {
         System.out.print("Nhập hệ số b: ");
         int b = sc.nextInt();
         // gọi x là nghiệm cảu phương trình
+        if (a != 0) {
+            double x = (double) -b/a;
+            System.out.println("Nghiệm của phương trình bậc nhất 1 ẩn là: " + x);
+
+        } else {
+            System.out.println("Không thể tính toán");
+        }
         
-        double x = (double) -b/a;
         
-        System.out.println("Nghiệm của phương trình bậc nhất 1 ẩn là: " + x);
     }
  
 }
--- src/Baitap/Bai06.java
+++ src/Baitap/Bai06.java
@@ -19,22 +19,33 @@ public class Bai06 {
         int b = sc.nextInt();
         System.out.print("Nhập hệ số c: ");
         int c = sc.nextInt();
-        int delta = b*b - 4*a*c;
-        if (delta > 0) {
-            // gọi x1, x2 là nghiệm của phương trình
-            double x1;
-            double x2;
-            System.out.println("Phương trình có 2 nghiệm phân biệt");
-            x1 = (double) (-b + Math.sqrt(delta)) / (2.0*a); 
-            x2 = (double) (b + Math.sqrt(delta)) / (2.0*a);
-            System.out.println("x1 = " + x1 +"\n" + "x2 = " + x2);
-        } else if (delta == 0) {
-            System.out.println("Phương trình có nghiệm duy nhất");
-            double x = - b / (2*a);
-            System.out.println("x = " + x);
+        
+        if (a != 0) {
+            int delta = b*b - 4*a*c;
+            if (delta > 0) {
+                // gọi x1, x2 là nghiệm của phương trình
+                double x1;
+                double x2;
+                System.out.println("Phương trình có 2 nghiệm phân biệt");
+                x1 = (double) (-b + Math.sqrt(delta)) / (2.0*a); 
+                x2 = (double) (b + Math.sqrt(delta)) / (2.0*a);
+                System.out.println("x1 = " + x1 +"\n" + "x2 = " + x2);
+            } else if (delta == 0) {
+                System.out.println("Phương trình có nghiệm duy nhất");
+                double x = - b / (2*a);
+                System.out.println("x = " + x);
+            } else {
+                System.out.println("Phương trình vô nghiệm");
+            }
         } else {
-            System.out.println("Phương trình vô nghiệm");
+            if (b == 0) {
+                System.out.println("Phương tình vô nghiệm");
+            } else {
+                double x = (double) -c/b;
+                System.out.println("Nghiệm của phương trình là: " + x);
+            }
         }
+        
                 
     }
 }
