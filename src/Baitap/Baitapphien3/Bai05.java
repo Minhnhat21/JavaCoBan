29b7a04bc92769c74ac0308c9051e1f61f5deedf
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2021-09-29 15:46:55 +0700

    add new file

---
+++ src/Baitap/Baitapphien3/Bai05.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Baitap.Baitapphien3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nguye
+ */
+import java.util.Scanner;
+public class Bai05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        float timeOfSeconds;
+	float mps,kph, mph;
+
+	System.out.print("Enter distance in meters: ");
+	float distance = sc.nextFloat();
+
+	System.out.print("Enter a hour: ");
+	float hr = sc.nextFloat();
+
+	System.out.print("Enter a minutes: ");
+	float min = sc.nextFloat();
+
+	System.out.print("Enter a seconds: ");
+	float sec = sc.nextFloat();
+
+	timeOfSeconds = (hr*3600) + (min*60) + sec;
+	mps = distance / timeOfSeconds;
+	kph = ( distance/1000.0f ) / ( timeOfSeconds/3600.0f );
+	mph = kph / 1.609f;
+
+	System.out.println("Your speed in meters/second is " + mps);
+	System.out.println("Your speed in km/h is " + kph);
+	System.out.println("Your speed in miles/h is "+ mph);
+
+    }
+}
